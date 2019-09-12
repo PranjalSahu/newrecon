71557fcf8cd7133ba4308952eb9265b25249e705
Pranjal Sahu <pranjalsahu5@gmail.com>
2019-09-12 18:54:28 -0400

    adding the code for building the sparse matrix for calculating the gradient

--- ReconDBT/SART_dbt.m
+++ ReconDBT/SART_dbt.m
@@ -20,34 +20,6 @@ function [img,cost] = SART_dbt(Gt,proj,x0,niter,stepsize,saveiter)
 %       of all the iterations.
 %   cost: a niterx1 vector containing the cost function value at each iteration
 %
-% Author: Rongping Zeng, FDA/CDRH/OSEL/DIDSR, 
-% Contact: rongping.zeng@fda.hhs.gov
-% Feb. 2018
-% Reference: Zeng EtAl, "Evaluating the sensitivity of the optimization of 
-% acquisition geometry to the choice of reconstruction algorithm in digital
-% breast tomosynthesis through a simulation study", PMB, vol. 60(3), 2015.
-
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%                       Legal Disclaimer
-% This software and documentation (the "Software") were developed at the 
-% Food and Drug Administration (FDA) by employees of the Federal Government 
-% in the course of their official duties. Pursuant to Title 17, Section 105
-% of the United States Code, this work is not subject to copyright protection
-% and is in the public domain. Permission is hereby granted, free of charge, 
-% to any person obtaining a copy of the Software, to deal in the Software 
-% without restriction, including without limitation the rights to use, copy, 
-% modify, merge, publish, distribute, sublicense, or sell copies of the 
-% Software or derivatives, and to permit persons to whom the Software is 
-% furnished to do so. FDA assumes no responsibility whatsoever for use by 
-% other parties of the Software, its source code, documentation or compiled 
-% executables, and makes no guarantees, expressed or implied, about its 
-% quality, reliability, or any other characteristic. Further, use of this 
-% code in no way implies endorsement by the FDA or confers any advantage in 
-% regulatory decisions. Although this software can be redistributed and/or 
-% modified freely, we ask that any derivative works bear some notice that 
-% they are derived from it, and any modified versions bear some notice that 
-% they have been modified.
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 if(nargin<6)
     saveiter=0;
@@ -70,51 +42,71 @@ else
     img=zeros([size(x0) 1]);
 end
 
-y=zeros(ns,nt);
+y = zeros(ns,nt);
+x = permute(x0, [1 3 2]);
 
-x=permute(x0,[1 3 2]);
+
 for iter=1:niter
     iter
     for i=1:nview
-        i
-        G=Gt{i};
-%         arg=G.arg;
-%         f3d_mex('init', arg.sys_type, uint8(arg.mask), ...
-%             int32(arg.nx), int32(arg.ny), int32(arg.nz), ...
-%              int32(arg.nthread), int32(arg.chat));
-         l=sum(G');
-         l=reshape(l,ns,nt);
-         lmask=logical(l>5*dz);%Mask out too small l value to avoid blowup in y:
-                     %some l can be very small. When it is divided from p, 
-                     %the y value can be overamplified at the boundary(the 
-                     %boundary value could be 10 or 20 times larger than the
-                     %normal object value.
-         pdif=proj(:,:,i)-G*x;
-         y(lmask)=pdif(lmask)./l(lmask);
+        
+        G = Gt{i};
+
+        l     = sum(G');
+        l     = reshape(l,ns,nt);
+        lmask = logical(l>5*dz);
+         
+         %Mask out too small l value to avoid blowup in y:
+         %some l can be very small. When it is divided from p, 
+         %the y value can be overamplified at the boundary(the 
+         %boundary value could be 10 or 20 times larger than the
+         %normal object value.
+         
+         pdif     = proj(:,:,i)-G*x;
+         y(lmask) = pdif(lmask)./l(lmask);
     %     y(isnan(y))=0;
+    
          imgi   = G'*y(:);
          denomi = sum(G);
          imgj   = imgi./denomi(:);
          imgj(isnan(imgj))=0;
-         x = x + stepsize*reshape(imgj,size(x));            
+         
+         x = x + stepsize*reshape(imgj, size(x));            
     end
+    
+    % Code to extract the elements from matrix given the coordinates
+    % 
+    % t1    = [1:numel(x)];              % get coordinate array
+    % t2    = t1+800;
+    % t2(t2 > numel(x)) = 1;
+    % s1 = sparse(index_arr, t1, v1, numel(x), numel(x));
+    % s2 = sparse(index_arr, t2, v1-2, numel(x), numel(x));
+    
+    % t2    = reshape(t1, size(x));
+    % x1    = reshape(x, [1, numel(x)]); % vectorize the phantom data
+    % temp  = x1(t2);                    % extract the data using the coordinate array
+    % temp1 = reshape(temp, size(x));    % reshape it to original size
+    % imshow(x(1:400, :, 224))           % show the image
     
     x(x<0)=0;
     %calculate the cost
     if(nargout>1)
         mse=0;
         for i=1:nview
-            pdif=proj(:,:,i)-Gt{i}*x;
-            mse=mse + sum(pdif(:).^2);
+            pdif   = proj(:,:,i)-Gt{i}*x;
+            mse    = mse + sum(pdif(:).^2);
         end
         cost(iter)=mse;
-    end     
+    end
+    
     if (saveiter)
-        img(:,:,:,iter)=permute(x,[1 3 2]);
+        img(:,:,:,iter) = permute(x,[1 3 2]);
     end
 end
+
 %img=embed(img./denom(:),arg.mask);
 %img=embed(x,arg.mask);
+
 if(~saveiter)
-    img=permute(x,[1 3 2]);
+    img = permute(x,[1 3 2]);
 end
