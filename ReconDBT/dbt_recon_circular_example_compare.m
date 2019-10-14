efc2a58cf8a6d3624e1dbb5f36199d893fc6d836
Pranjal Sahu <pranjalsahu5@gmail.com>
2019-10-14 11:24:47 -0400

    adding the code to compare the recon of 110 angle range projections

--- ReconDBT/dbt_recon_circular_example_compare.m
+++ ReconDBT/dbt_recon_circular_example_compare.m
@@ -16,22 +16,22 @@ load g_noi.mat;    % the variable is 'g_noi'.
 
 
 % Good Cases to see
-% 162, 163, 166
+% 162, 163, 166, 172
 
-load '/media/dril/ubuntudata/DBT-NEW/gan-90-projections-higher/projections/g_noi_nonoise_162.mat';
+load '/media/dril/ubuntudata/DBT-NEW/gan-110-projections/projections/g_noi_nonoise_172.mat';
 a        = g_noi_nonoise;
-[x1, s1, c1] = do_recon(a, 91.6666);
+[x1, s1, c1] = do_recon(a, 112.4982);
 
 
-load '/media/dril/ubuntudata/DBT-NEW/gan-90-projections-higher/predictions/model-sinogram32-higher-dril/22_prediction.mat';
+load '/media/dril/ubuntudata/DBT-NEW/gan-110-projections/predictions/32_prediction.mat';
 b        = prediction;
 b        = double(b);
-[x2, s2, c2] = do_recon(b, 91.6666);
+[x2, s2, c2] = do_recon(b, 112.4982);
 
 
-load '/media/dril/ubuntudata/DBT-NEW/gan-90-projections-higher/projections/g_noi_162.mat';
+load '/media/dril/ubuntudata/DBT-NEW/gan-110-projections/projections/g_noi_172.mat';
 c        = g_noi;
-c        = c(:, :, 11:35); 
+c        = c(:, :, 16:40); 
 [x3, s3, c3] = do_recon(c, 50);
 
 load head.mat;
@@ -39,10 +39,38 @@ load head.mat;
 disp('Cost are');
 disp([c1, c2, c3]);
 
+%hold on;
+figure
+
 t1 = (s1(:, :, sliceindex) + s1(:, :, sliceindex+1) + s1(:, :, sliceindex-1))/3;
 t2 = (s2(:, :, sliceindex) + s2(:, :, sliceindex+1) + s2(:, :, sliceindex-1))/3;
 t3 = (s3(:, :, sliceindex) + s3(:, :, sliceindex+1) + s3(:, :, sliceindex-1))/3;
+t4 = (x(:, :, sliceindex) + x(:, :, sliceindex+1) + x(:, :, sliceindex-1))/3;
 imshow([t1 t2 t3]);
+
+hold on;
+figure
+
+p1 = (x1(:, :, sliceindex) + x1(:, :, sliceindex+1) + x1(:, :, sliceindex-1))/3;
+p2 = (x2(:, :, sliceindex) + x2(:, :, sliceindex+1) + x2(:, :, sliceindex-1))/3;
+p3 = (x3(:, :, sliceindex) + x3(:, :, sliceindex+1) + x3(:, :, sliceindex-1))/3;
+imshow([p1 p2 p3]);
+
+hold on;
+figure
+ 
+q1 = reshape(s1(:, sliceindex, :), [400, 160]);
+q2 = reshape(s2(:, sliceindex, :), [400, 160]);
+q3 = reshape(s3(:, sliceindex, :), [400, 160]);
+imshow([q1 q2 q3]);
+
+hold on;
+figure
+
+k1 = reshape(x1(:, sliceindex, :), [400, 160]);
+k2 = reshape(x2(:, sliceindex, :), [400, 160]);
+k3 = reshape(x3(:, sliceindex, :), [400, 160]);
+imshow([k1 k2 k3]);
 
 function [xfbp, xartt, costart] = do_recon(g, orbit_angle)
      nview = size(g, 3);
@@ -61,8 +89,8 @@ function [xfbp, xartt, costart] = do_recon(g, orbit_angle)
 
      orbit = orbit_angle;     % in degree: angular span
      na = size(g,3); % number of projection views
-     ds = 0.0255;      % in cm: detector element pixel size in the 's' direction; 
-     dt = 0.0255;      % in cm: detector element pixel size in the 's' direction; 
+     ds = 0.04;      % in cm: detector element pixel size in the 's' direction; 
+     dt = 0.04;      % in cm: detector element pixel size in the 's' direction; 
                      % 's', the x-ray tube moving direction, positive pointing toward right.           
                      % 't', the perpendicular direction to 's' direction, positive pointing toward the nipple.            
 
@@ -73,12 +101,12 @@ function [xfbp, xartt, costart] = do_recon(g, orbit_angle)
      d_objbottom_det = 0;  % in cm,the distance from the bottom of the object to the center detector.
     
 
-    nrx      = 800;
-    nry      = 448;
-    drx      = 0.0255;  
+    nrx      = 400;
+    nry      = 224;
+    drx      = 0.04;  
     dry      = drx; 
-    drz      = 0.0255; 
-    nrz      = 320; 
+    drz      = 0.04; 
+    nrz      = 160; 
     offset_x = 0; %in pixels
     offset_y = -nry/2;% in pixels. 0 for full cone, -nry/2 for half cone
     zfov     = nrz*drz;
@@ -113,6 +141,11 @@ function [xfbp, xartt, costart] = do_recon(g, orbit_angle)
      %xbp = BP(Gtr, g); % initialization for SART
      %disp(size(xbp));
      
-     [xartt, costart] = SART_dbt(Gtr,  g, zeros(800, 448, 320), 1, 0.9);
+     %[xartt, costart] = SART_dbt(Gtr,  g, xbp, 1, 0.9);
+     [xartt, costart] = SART_dbt(Gtr,  g, zeros(400, 224, 160), 1, 0.9);
 end
 
+
+%fid=fopen('s3.raw','w+');
+%cnt=fwrite(fid,s3,'float');
+%fclose(fid);
